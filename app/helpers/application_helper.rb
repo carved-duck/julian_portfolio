ec8ad765faad66c6414f535880d7651198d8c5a4
Julian Schoenfeld <juliankick@gmail.com>
2025-06-19 10:33:40 +0900

    fixed external link breaking issues. Fixed the last of cloudinary breaking issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def format_url(url, add_www: false)
+    return nil if url.blank?
+
+    if url.start_with?('http://', 'https://')
+      url
+    elsif add_www && !url.start_with?('www.')
+      "https://www.#{url}"
+    elsif url.start_with?('www.')
+      "https://#{url}"
+    else
+      "https://#{url}"
+    end
+  end
 end
