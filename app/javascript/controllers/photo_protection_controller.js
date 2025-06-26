d9a881865d029063985251149cde522233db6cac
Julian Schoenfeld <juliankick@gmail.com>
2025-06-26 14:50:40 +0900

    added photo protection to stop downloading of photos

---
+++ app/javascript/controllers/photo_protection_controller.js
@@ -0,0 +1,110 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["image"]
+
+    connect() {
+    this.element.addEventListener('contextmenu', this.preventRightClick.bind(this))
+    this.element.addEventListener('dragstart', this.preventDrag.bind(this))
+    this.element.addEventListener('selectstart', this.preventSelect.bind(this))
+
+    // Disable right-click on all images in this element
+    const images = this.element.querySelectorAll('img')
+    images.forEach(img => {
+      img.addEventListener('contextmenu', this.preventRightClick.bind(this))
+      img.addEventListener('dragstart', this.preventDrag.bind(this))
+      img.addEventListener('selectstart', this.preventSelect.bind(this))
+
+      // Mobile-specific protection
+      this.addMobileProtection(img)
+
+      // Disable common keyboard shortcuts
+      img.addEventListener('keydown', this.preventKeyboardShortcuts.bind(this))
+    })
+
+    // Disable keyboard shortcuts on the container
+    this.element.addEventListener('keydown', this.preventKeyboardShortcuts.bind(this))
+  }
+
+  disconnect() {
+    this.element.removeEventListener('contextmenu', this.preventRightClick.bind(this))
+    this.element.removeEventListener('dragstart', this.preventDrag.bind(this))
+    this.element.removeEventListener('selectstart', this.preventSelect.bind(this))
+    this.element.removeEventListener('keydown', this.preventKeyboardShortcuts.bind(this))
+  }
+
+  preventRightClick(event) {
+    event.preventDefault()
+    return false
+  }
+
+  preventDrag(event) {
+    event.preventDefault()
+    return false
+  }
+
+  preventSelect(event) {
+    event.preventDefault()
+    return false
+  }
+
+    preventKeyboardShortcuts(event) {
+    // Disable Ctrl+S (Save), Ctrl+A (Select All), Ctrl+C (Copy), etc.
+    if (event.ctrlKey || event.metaKey) {
+      if (event.key === 's' || event.key === 'a' || event.key === 'c' ||
+          event.key === 'S' || event.key === 'A' || event.key === 'C') {
+        event.preventDefault()
+        return false
+      }
+    }
+
+    // Disable F12 (Developer Tools)
+    if (event.key === 'F12') {
+      event.preventDefault()
+      return false
+    }
+  }
+
+  addMobileProtection(img) {
+    // Prevent long-press context menu on mobile
+    img.addEventListener('touchstart', this.preventLongPress.bind(this), { passive: false })
+    img.addEventListener('touchend', this.preventLongPress.bind(this), { passive: false })
+    img.addEventListener('touchcancel', this.preventLongPress.bind(this), { passive: false })
+
+    // Prevent callout (iOS Safari image save popup)
+    img.style.webkitTouchCallout = 'none'
+    img.style.webkitUserSelect = 'none'
+
+    // Add pointer events prevention for mobile
+    img.addEventListener('pointerdown', this.preventPointerEvents.bind(this))
+    img.addEventListener('pointerup', this.preventPointerEvents.bind(this))
+
+    // Prevent image zoom/scaling on mobile
+    img.addEventListener('gesturestart', this.preventGesture.bind(this))
+    img.addEventListener('gesturechange', this.preventGesture.bind(this))
+    img.addEventListener('gestureend', this.preventGesture.bind(this))
+  }
+
+  preventLongPress(event) {
+    // Prevent long press context menu on mobile
+    if (event.target.tagName === 'IMG') {
+      event.preventDefault()
+      event.stopPropagation()
+      return false
+    }
+  }
+
+  preventPointerEvents(event) {
+    // Prevent pointer events that could lead to save actions
+    if (event.target.tagName === 'IMG') {
+      event.preventDefault()
+      return false
+    }
+  }
+
+  preventGesture(event) {
+    // Prevent pinch-to-zoom and other gestures on images
+    event.preventDefault()
+    return false
+  }
+}
